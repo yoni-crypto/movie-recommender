4d16465cf621f60894bd80105ace9dd24dae7485
yoni-crypto <yonidisu111@gmail.com>
2025-01-28 14:43:34 +0300

    updated the movie card and movie list

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => (
-    <nav className="bg-gray-800 text-white py-4 px-8 flex justify-between items-center">
+    <nav className="bg-gray-800 text-white py-2 lg:py-4 px-8 flex justify-between items-center">
         <h1 className="text-2xl font-bold">
             <Link to="/">Movie<span className="text-blue-500">Hub</span></Link>
         </h1>
